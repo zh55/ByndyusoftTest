aaedbfe9c7fe41c7df0a9140d57bea6c81cbe3a7
zhilkin <azhilkin@gmini.tech>
2024-11-01 11:03:53 +0300

    fix

--- cypress/e2e/e2e_test_checkPhone.cy.js
+++ cypress/e2e/e2e_test_checkPhone.cy.js
@@ -12,7 +12,7 @@ describe('Проверка контактной информации на сай
             // const phone = "8 800 775-15-21"
             const contactEmail = "sales@byndyusoft.com"
 
-            cy.contains("Заказать презентацию").click();
+            cy.get("[class=\"knowMore__container\"]").contains("Заказать презентацию").click();
             // cy.contains(phone).should('be.visible');                       //Его сейчас нет на странице, убрали? бага?
             cy.contains(contactEmail).should('be.visible');
                 })
